a30bedcf81b1d1244818857447077a0206ce9601
Jake Dudum <jakedudum@gmail.com>
2019-06-27 17:36:44 -0700

    Added print inv function to bamazonSUpervisor.js and added a supervisor view with inquirer as well as started work on viewing departments sales profit

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -15,3 +15,83 @@ var connection = mysql.createConnection({
     password: "password",
     database: "bamazon"
 });
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    supervisorView();
+});
+
+function supervisorView() {
+    viewProducts();
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "What would you like to do?",
+            choices: ["View Product Sales by Department", "Create New Department", "Quit"],
+            name: "selection"
+        }
+    ])
+        .then(function (response) {
+            switch (response.selection) {
+                case "View Product Sales by Department":
+                    viewProductSales();
+                    break;
+                case "Create New Department":
+                    createDepartment();
+                    break;
+                case "Quit":
+                    connection.end();
+                    break;
+            }
+        });
+}
+
+function viewProducts() {
+    var table = new Table({
+        head: ['ID', 'Product', 'Department', 'Price', 'Stock']
+        , colWidths: [5, 65, 25, 10, 10],
+        chars: {
+            'top': '═', 'top-mid': '╤', 'top-left': '╔', 'top-right': '╗'
+            , 'bottom': '═', 'bottom-mid': '╧', 'bottom-left': '╚', 'bottom-right': '╝'
+            , 'left': '║', 'left-mid': '╟', 'mid': '─', 'mid-mid': '┼'
+            , 'right': '║', 'right-mid': '╢', 'middle': '│'
+        }
+    });
+
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        for (var i = 0; i < res.length; i++) {
+            table.push([res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]);
+        }
+        console.log("\n" + table.toString() + "\n");
+        supervisorView();
+    });
+}
+
+function viewProductSales() {
+    var table = new Table({
+        head: ['ID', 'Product', 'Department', 'Price', 'Stock']
+        , colWidths: [5, 65, 25, 10, 10],
+        chars: {
+            'top': '═', 'top-mid': '╤', 'top-left': '╔', 'top-right': '╗'
+            , 'bottom': '═', 'bottom-mid': '╧', 'bottom-left': '╚', 'bottom-right': '╝'
+            , 'left': '║', 'left-mid': '╟', 'mid': '─', 'mid-mid': '┼'
+            , 'right': '║', 'right-mid': '╢', 'middle': '│'
+        }
+    });
+
+    connection.query("SELECT departments.department_id, departments.department_name, departments.over_head_costs FROM departments LEFT JOIN products WHERE departments.department_name = products.department_name", function (err, res) {
+        if (err) throw err;
+
+        for (var i = 0; i < res.length; i++) {
+            table.push([res[i].department_id, res[i].department_name, res[i].over_head_costs, res[i].productsales, res[i].profit]);
+        }
+        console.log("\n" + table.toString() + "\n");
+        managerView();
+    });
+}
+
+function createDepartment() {
+
+}
